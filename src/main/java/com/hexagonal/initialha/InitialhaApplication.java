840ca540c8efdf35d8af871c5988c126c1b92330
ajranchal <ajranchal@gmail.com>
2021-06-14 10:35:46 +0200

    Initial commit

---
+++ src/main/java/com/hexagonal/initialha/InitialhaApplication.java
@@ -0,0 +1,13 @@
+package com.hexagonal.initialha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitialhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitialhaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hexagonal/initialha/InitialhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexagonal.initialha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitialhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
