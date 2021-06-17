57f627a5edd0f8b7630cc9f437a0e539198e1613
ajranchal <ajranchal@gmail.com>
2021-06-17 15:40:49 +0200

    Remove helloworld

--- src/main/java/com/hexagonal/domain/model/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package com.hexagonal.domain.model;
-
-import lombok.Value;
-
-@Value
-public class HelloWorld {
-
-    private String hello;
-}
--- src/main/java/com/hexagonal/domain/ports/primary/HelloWorldPrimaryPort.java
+++
@@ -1,8 +0,0 @@
-package com.hexagonal.domain.ports.primary;
-
-import java.util.Optional;
-
-public interface HelloWorldPrimaryPort {
-
-    Optional<String> helloWorld(String language);
-}
